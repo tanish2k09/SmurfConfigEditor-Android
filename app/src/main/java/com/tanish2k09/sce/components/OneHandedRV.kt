70eba5a4726e5b54ee04daf262628054a17c6bbb
Tanish Manku <tanish2k09.dev@gmail.com>
2020-04-28 23:43:29 -0700

    One-handed recycler view was dropped, delete it
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- app/src/main/java/com/tanish2k09/sce/components/OneHandedRV.kt
+++
@@ -1,8 +0,0 @@
-package com.tanish2k09.sce.components
-
-import android.content.Context
-import androidx.recyclerview.widget.RecyclerView
-
-class OneHandedRV(private val ctx: Context) : RecyclerView(ctx) {
-
-}
